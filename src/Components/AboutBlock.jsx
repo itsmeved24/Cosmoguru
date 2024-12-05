2108ab3ad31f08fea6acc1bd5c812c9cd5da8c83
itsmeved24 <vedankvansia@gmail.com>
2024-12-06 01:11:23 +0530

    correct

--- src/Components/AboutBlock.jsx
+++ src/Components/AboutBlock.jsx
@@ -4,9 +4,9 @@ import Block from "./Block";
 const AboutBlock = () => (
   <Block className="w-1/2 h-70 col-span-12 text-3xl leading-snug mt-2">
     <h1 className="mb-12 text-2xl font-medium leading-tight">
-      My passion is building <span className="text-zinc-400">cool stuff (⌐■_■)</span>.{" "}
-
-      I am a <span className="text-zinc-400"> Creative Graphic Web Developer </span>with taste in <span className="text-zinc-400">art 🖼️, visual appearance 🔮, and an eye for user interactivity 📲</span>!
+      My passion is building <span className="text-zinc-400">cool things (⌐■_■)</span>.{" "}
+      <br />
+      I am a <span className="text-zinc-400"> Creative Graphic Web Developer </span>with <span className="text-zinc-400">taste for art 🖼️, a keen sense of visual aesthetics 🔮, and an eye for user interactivity 📲</span>!
     </h1>
   </Block>
 );
